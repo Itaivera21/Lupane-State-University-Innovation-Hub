8226341b0d47cc6174da2d295eae58e461ab90b7
Itaivera21 <itaivera21@gmail.com>
2026-05-15 16:39:44 +0200

    new commit

--- app.py
+++ app.py
@@ -38,8 +38,8 @@ app.config['SECRET_KEY'] = 'your-secret-key-here'
 # MySQL connection string for TiDB Cloud
 # Format: mysql+pymysql://username:password@host:port/database?charset=utf8mb4
 app.config['SQLALCHEMY_DATABASE_URI'] = (
-    'mysql+pymysql://Zs5lycD7dYgEUy3rOot:qar204jhgxpJE2sB@'
-    'gateway0teu-central-tprodaws.tidbcloud.com:4000/innovation_hub'
+    'mysql+pymysql://Zs51ycD7dYgEUy3.root:qar204jhgxpJE2sB@'
+    'gateway01.eu-central-1.prod.aws.tidbcloud.com:4000/innovation_hub'
     '?charset=utf8mb4'
 )
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False

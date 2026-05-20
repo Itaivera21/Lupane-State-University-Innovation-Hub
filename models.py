eaf282af42c76ef1a76b0f110f72a19bb1ae3b45
Itaivera21 <itaivera21@gmail.com>
2026-05-20 07:46:01 +0200

    new commit

--- models.py
+++ models.py
@@ -11,7 +11,8 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
+    # FIXED: Changed from String(200) to String(512) to prevent hash truncation
+    password = db.Column(db.String(512), nullable=False)
     faculty = db.Column(db.String(100), nullable=True)
     student_id = db.Column(db.String(50), unique=True, nullable=True)
     skills = db.Column(db.Text, default='')
